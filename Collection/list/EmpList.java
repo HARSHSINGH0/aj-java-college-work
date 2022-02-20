22c7a2199045694b5477491afde8f355e85edea4
HARSH SINGH <41187121+HARSHSINGH0@users.noreply.github.com>
2022-02-20 13:55:49 +0530

    Update EmpList.java

--- Collection/list/EmpList.java
+++ Collection/list/EmpList.java
@@ -17,6 +17,7 @@ class EmpList{
         Employee e1=new Employee(101,"ABC");
         Employee e2=new Employee(102,"PQR");
         Employee e3=new Employee(103,"XYZ");
+        Employee e4=new Employee(104,"XYZ1");
 
         //add an element to the list
         list1.add(e1);
@@ -26,6 +27,8 @@ class EmpList{
         list1.add(1,e3);
         //remove at index position
         list1.remove(2);
+        //set data at specfic location
+        list1.set(1,e4);
         //collection based for loop to traverse
         for(Employee e:list1){
             System.out.println(e.toString());
